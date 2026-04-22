c722e7ee15a6fe64ba0cbc27f8dd302c12717fbe
RedWlf1999 <matefejer999@gmail.com>
2026-04-22 19:56:50 +0200

    Elso lepesek

---
+++ Controllers/Feladatok.cs
@@ -0,0 +1,33 @@
+﻿namespace Controllers
+{
+    public class Feladatok
+
+    {
+        public void OraInditas()
+        {
+            Console.WriteLine("Tanóra indítás:");
+        }
+        public void OraBefejezes()
+        {
+            Console.WriteLine("Tanóra befejezése:");
+        }
+        public void OraRiportGeneralas()
+        {
+            Console.WriteLine("Tanóra riport generálása:");
+        }
+        public void JelenletKezeles()
+        {
+            Console.WriteLine("Jelenlét nyilvantartó:");
+        }
+        public void JegyKezeles()
+        {
+            Console.WriteLine("Jegyek kezelése:");
+        }
+        public void DiakokKezelese()
+        {
+            Console.WriteLine("Diákok kezelése:");
+        }
+
+
+    }
+}
---
+++ IskolaiJelenlet/Program.cs
@@ -0,0 +1,51 @@
+﻿using Controllers;
+
+namespace IskolaiJelenlet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var menu = 0;
+            var Controllerek = new Feladatok();
+            
+            do
+            {
+                Console.Write("1: Óra indítás\n2: Óra befejezés" +
+                    "\n3: Diákok kezelése\n4: Jegy kezelő\n5: Jelenlét kezelő\n0: Kilépés\nKérem válasszon menüpontot:");
+                var scanner = Console.ReadLine();
+                menu=Convert.ToInt32(scanner);
+                Console.Clear();
+            } while ( (menu < 0) || (menu > 5) );
+            Console.Clear();
+            switch (menu)
+            {
+                case 1:
+                    Controllerek.OraInditas();
+                    break;
+                case 2:
+                    Controllerek.OraBefejezes();
+                    break;
+                case 3:
+                    Controllerek.DiakokKezelese();
+                    break;
+                case 4:
+                    Controllerek.JegyKezeles();
+                    break;
+                case 5:
+                    Controllerek.JelenletKezeles();
+                    break;
+                default:
+                    break;
+            }
+                
+            
+            
+            
+
+
+
+        }
+    }
+}
